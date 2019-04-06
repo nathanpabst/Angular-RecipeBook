7f17a709a536ab590e2774729ffd9f07ffa21a34
Nathan Pabst <Nathan.Pabst@ey.com>
2019-04-06 12:27:04 -0500

    added navigation with event binding and *ngIf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }

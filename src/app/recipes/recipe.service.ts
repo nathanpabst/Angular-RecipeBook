1d0f745b68703a8a031f10f81430ece4fb260c5c
Nathan Pabst <Nathan.Pabst@ey.com>
2019-05-03 18:56:23 -0500

    refactored code to use Subject instead of EventEmitter.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,11 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
         new Recipe(
             'Tasty Schnitzel',
@@ -24,7 +23,6 @@ export class RecipeService {
                 new Ingredient('Buns', 2),
                 new Ingredient('Meat', 1)
             ])
-            
       ];
 
       constructor(private shoppingListService: ShoppingListService) {

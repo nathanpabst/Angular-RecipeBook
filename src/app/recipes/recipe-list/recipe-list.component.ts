73d85682e6dc9b6c2eb57b5c465d0c798ab4ec97
Nathan Pabst <Nathan.Pabst@ey.com>
2019-05-06 19:55:31 -0500

    submitting a recipe works as expected

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
- 
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,18 +10,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class RecipeListComponent implements OnInit {
  recipes: Recipe[];
-  
 
   constructor(private recipeService: RecipeService,
-    private router: Router,
-    private route: ActivatedRoute) {}
+              private router: Router,
+              private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.recipeService.recipesChanged
+    .subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }
+    );
     this.recipes = this.recipeService.getRecipes();
   }
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
-
   }
 }

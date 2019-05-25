70cc3e19482d0a75a9d9ee7e1a0c5ba498510534
Nathan Pabst <Nathan.Pabst@ey.com>
2019-05-25 13:26:13 -0500

    added firebase for auth.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD7dfjvKWFqx-fYeUopXk6bnvgIg5PwcoI',
+      authDomain: 'angular-recipebook-bb4a1.firebaseapp.com'
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;

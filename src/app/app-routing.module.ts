2b970ea5e16c19a28ec11ed6f390392f81b19d70
Nathan Pabst <Nathan.Pabst@ey.com>
2019-05-25 13:02:37 -0500

    added registration page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -13,10 +14,10 @@ const appRoutes: Routes = [
         { path: 'new', component: RecipeEditComponent },
         { path: ':id', component: RecipeDetailComponent },
         { path: ':id/edit', component: RecipeEditComponent }
-
     ] },
     { path: 'shopping-list', component: ShoppingListComponent },
-]
+    { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],

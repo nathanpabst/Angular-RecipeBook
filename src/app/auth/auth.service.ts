d09df36dfadcc23bb6d912d05e90ac472299c268
Nathan Pabst <Nathan.Pabst@ey.com>
2019-06-23 13:28:09 -0500

    code in place to allow new users to sign up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+
 export class AuthService {
+    constructor(private http: HttpClient) { }
+
     signupUser(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD7dfjvKWFqx-fYeUopXk6bnvgIg5PwcoI',
+            {
+                email,
+                password,
+                returnSecureToken: true
+            });
     }
 }

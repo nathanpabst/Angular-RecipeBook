199f781e59fd62978e2604bfaaf3789d81bac9d5
Nathan Pabst <Nathan.Pabst@ey.com>
2019-06-23 19:07:52 -0500

    wip

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -19,16 +21,24 @@ export class AuthComponent {
     }
 
     onSubmit(form: NgForm) {
-        // console.log(form.value);
         if (!form.valid) {
             return;
         }
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObs: Observable<AuthResponseData> 
+
         this.isLoading = true;
         if (this.isLoginMode) {
-            // ...
+            this.authService.login(email, password).subscribe(resData => {
+                console.log(resData);
+                this.isLoading = false;
+            }, errorMessage => {
+                console.log(errorMessage);
+                this.error = errorMessage;
+                this.isLoading = false;
+            });
         } else {
             this.authService.signupUser(email, password).subscribe(resData => {
                 console.log(resData);
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -36,5 +37,17 @@ export class AuthService {
             }
             return throwError(errorMessage);
         }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyD7dfjvKWFqx-fYeUopXk6bnvgIg5PwcoI',
+            {
+                email,
+                password,
+                returnSecureToken: true,
+            }
+            );
+
     }
 }

bdde7e6cd0b7065de1fbcd763414303634e2bdd9
Nathan Pabst <Nathan.Pabst@ey.com>
2019-09-22 09:55:34 -0500

    refactoring login action to include signing up.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -95,7 +95,7 @@ export class AuthService {
     );
     if (loadedUser.token) {
       this.store.dispatch(
-        new AuthActions.Login({
+        new AuthActions.AuthenticateSuccess({
           email: loadedUser.email,
           userId: loadedUser.id,
           token: loadedUser.token,
@@ -135,7 +135,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.store.dispatch(
-      new AuthActions.Login({
+      new AuthActions.AuthenticateSuccess({
         email,
         userId,
         token,

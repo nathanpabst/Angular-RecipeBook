88dfba2d3ef6e5154e8113117d34e654fa081ac9
Nathan Pabst <Nathan.Pabst@ey.com>
2019-10-06 10:12:43 -0500

    adding action for set recipes.

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,13 +1,25 @@
 import { Recipe } from '../recipe.model';
+import * as RecipesActions from './recipe.actions';
 
 export interface State {
-    recipes: Recipe[];
+  recipes: Recipe[];
 }
 
 const initialState: State = {
-    recipes: [],
+  recipes: []
 };
 
-export function recipeReducer(state, action) {
-    return state;
+export function recipeReducer(
+  state = initialState,
+  action: RecipesActions.RecipesActions
+) {
+  switch (action.type) {
+    case RecipesActions.SET_RECIPES:
+      return {
+        ...state,
+        recipes: [...action.payload]
+      };
+    default:
+      return state;
+  }
 }

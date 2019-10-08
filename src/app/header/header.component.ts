626af7cc680f7cce8d4c3bad9c0b461fef6e9995
Nathan Pabst <Nathan.Pabst@ey.com>
2019-10-08 17:21:48 -0500

    setup for fetching recipes via the ngrx store complete.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
+import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Component({
   selector: 'app-header',
@@ -29,8 +30,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
       .pipe(map(authState => authState.user))
       .subscribe(user => {
         this.isAuthenticated = !!user;
-        console.log('from header: ', !user);
-        console.log('from header: ', !!user);
       });
     }
 
@@ -39,7 +38,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 
   onLogout() {
---
+++ src/app/recipes/store/recipe.effects.ts
@@ -0,0 +1,34 @@
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { switchMap, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+import * as RecipesActions from './recipe.actions';
+import { Recipe } from '../recipe.model';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class RecipeEffects {
+  @Effect()
+  fetchRecipes = this.actions$.pipe(
+    ofType(RecipesActions.FETCH_RECIPES),
+    switchMap(() => {
+      return this.http.get<Recipe[]>(
+        'https://angular-recipebook-bb4a1.firebaseio.com/recipes.json'
+      );
+    }),
+    map(recipes => {
+        return recipes.map(recipe => {
+            return {
+                ...recipe,
+                ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+        });
+    }),
+    map(recipes => {
+        return new RecipesActions.SetRecipes(recipes);
+    })
+  );
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}

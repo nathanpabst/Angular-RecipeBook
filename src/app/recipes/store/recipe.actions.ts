2d5fdfacce8c0f92d29177ab4ed66dc7ce3b7140
Nathan Pabst <Nathan.Pabst@ey.com>
2019-10-08 19:03:21 -0500

    code in place for the recipe reducer.

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -9,32 +9,36 @@ export const UPDATE_RECIPE = '[Recipe] Update Recipe';
 export const DELETE_RECIPE = '[Recipe] Delete Recipe';
 
 export class SetRecipes implements Action {
-    readonly type = SET_RECIPES;
+  readonly type = SET_RECIPES;
 
-    constructor(public payload: Recipe[]) {
-    }
+  constructor(public payload: Recipe[]) {}
 }
 
 export class FetchRecipes implements Action {
-    readonly type = FETCH_RECIPES;
+  readonly type = FETCH_RECIPES;
 }
 
 export class AddRecipe implements Action {
-    readonly type = ADD_RECIPE;
+  readonly type = ADD_RECIPE;
 
-    constructor(public payload: Recipe) {}
+  constructor(public payload: Recipe) {}
 }
 
 export class UpdateRecipe implements Action {
-    readonly type = UPDATE_RECIPE;
+  readonly type = UPDATE_RECIPE;
 
-    constructor(public payload: {index: number; newRecipe: Recipe}) {}
+  constructor(public payload: { index: number; newRecipe: Recipe }) {}
 }
 
 export class DeleteRecipe implements Action {
-    readonly type = DELETE_RECIPE;
+  readonly type = DELETE_RECIPE;
 
-    constructor(public payload: number) {}
+  constructor(public payload: number) {}
 }
 
-export type RecipesActions = SetRecipes;
+export type RecipesActions =
+  | SetRecipes
+  | FetchRecipes
+  | AddRecipe
+  | UpdateRecipe
+  | DeleteRecipe;
--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,5 +1,6 @@
 import { Recipe } from '../recipe.model';
 import * as RecipesActions from './recipe.actions';
+import { act } from '@ngrx/effects';
 
 export interface State {
   recipes: Recipe[];
@@ -19,6 +20,38 @@ export function recipeReducer(
         ...state,
         recipes: [...action.payload]
       };
+    case RecipesActions.ADD_RECIPE:
+      return {
+        ...state,
+        recipes: [...state.recipes, action.payload]
+      };
+    case RecipesActions.UPDATE_RECIPE:
+      const updatedRecipe = {
+        // find the appropriate recipe to update && make a copy
+        ...state.recipes[action.payload.index],
+        // update/override the recipe
+      ...action.payload.newRecipe
+    };
+      // copy the list of recipes
+      const updatedRecipes = [...state.recipes];
+      // replace the updated recipe in the new list
+      updatedRecipes[action.payload.index] = updatedRecipe;
+
+      return {
+        ...state,
+        // return the new list of recipes
+        recipes: updatedRecipes
+      };
+    case RecipesActions.DELETE_RECIPE:
+      return {
+        ...state,
+        // filter function will return a new list
+        recipes: state.recipes.filter((recipe, index) => {
+          // if the recipe index does match the payload, the recipe will remain in the list
+          return index !== action.payload;
+        })
+      };
+
     default:
       return state;
   }

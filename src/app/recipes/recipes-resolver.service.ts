e967781fde99c2835f5888a50f5bacab057a65c4
Nathan Pabst <Nathan.Pabst@ey.com>
2019-10-08 19:37:17 -0500

    fixed issue in recipe resolver to prevent the server from overriding edits.

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Actions, ofType } from '@ngrx/effects';
-import { take } from 'rxjs/operators';
+import { take, switchMap, map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import * as fromApp from '../store/app.reducer';
@@ -14,8 +15,21 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
     constructor(private store: Store<fromApp.AppState>, private actions$: Actions ) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        this.store.dispatch(new RecipeActions.FetchRecipes());
-        return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+        return this.store.select('recipes').pipe(
+            take(1),
+            map(recipesState => {
+            return recipesState.recipes;
+        }),
+        switchMap(recipes => {
+            if (recipes.length === 0) {
+                this.store.dispatch(new RecipeActions.FetchRecipes());
+                return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1)
+                );
+            } else {
+                return of(recipes);
+            }
+        })
+        );
 
         // const recipes = this.recipeService.getRecipes();
 

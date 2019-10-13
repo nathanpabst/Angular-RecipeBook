5cecadebe5ffbf9a3be510e5977b6078e4cd1182
Nathan Pabst <Nathan.Pabst@ey.com>
2019-10-13 17:29:14 -0500

    refactored recipes to use ngrx store.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,6 @@ import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 
-import { DataStorageService } from '../shared/data-storage.service';
-import { AuthService } from '../auth/auth.service';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
 import * as RecipeActions from '../recipes/store/recipe.actions';
@@ -13,28 +11,23 @@ import * as RecipeActions from '../recipes/store/recipe.actions';
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(
-    private dataStorageService: DataStorageService,
-    private authService: AuthService,
-    private store: Store<fromApp.AppState>
-  ) { }
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
-      this.userSub = this.store
+    this.userSub = this.store
       .select('auth')
       .pipe(map(authState => authState.user))
       .subscribe(user => {
         this.isAuthenticated = !!user;
       });
-    }
+  }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes();
+    this.store.dispatch(new RecipeActions.StoreRecipes());
   }
 
   onFetchData() {
